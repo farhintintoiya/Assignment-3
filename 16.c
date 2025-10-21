16236a2ab02983699a6f408988b2798938912f42
Farhin Tintoiya <farhin.tintoiya29@gmail.com>
2025-10-21 18:35:08 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -3,7 +3,26 @@
 
 int main() {
 
-               
+   char sex;
+    int boys = 0, girls = 0;
+
+    printf("Enter sex code for 50 students (M/F): \n");
+
+    for(int i = 1; i <= 50; i++) {
+        printf("Student %d: ", i);
+        scanf(" %c", &sex);  
+        if(sex == 'M' || sex == 'm') {
+            boys++;
+        } else if(sex == 'F' || sex == 'f') {
+            girls++;
+        } else {
+            printf("Invalid input! Please enter M or F only.\n");
+            i--; 
+        }
+    }
+
+    printf("\nTotal boys: %d\n", boys);
+    printf("Total girls: %d\n", girls);       
 
     return 0;
 }
